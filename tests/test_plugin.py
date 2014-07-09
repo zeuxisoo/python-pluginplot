54c02a94e4b3f7b4768e829fe6427969c438e5e9
Zeuxis Lo <seekstudio@gmail.com>
2014-07-09 17:10:19 +0800

    add test case for plugin class

---
+++ tests/test_plugin.py
@@ -0,0 +1,65 @@
+#coding: utf-8
+
+from __future__ import print_function
+
+import sys
+import unittest
+from contextlib import contextmanager
+from StringIO import StringIO
+from pluginpress import Plugin
+
+@contextmanager
+def captured_output():
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
+
+class TestPlugin(unittest.TestCase):
+
+    def setUp(self):
+        self.plugin = Plugin()
+
+    def test_add_filter(self):
+        def strong(value):
+            return "<strong>{0}</strong>".format(value)
+
+        self.plugin.add_filter(name='strong', method=strong)
+
+        self.assertEquals(self.plugin.filters['strong'], strong)
+
+    def test_apply_filter(self):
+        strong  = lambda value: "<strong>{0}</strong>".format(value)
+        address = lambda value: "<address>{0}</address>".format(value)
+
+        self.plugin.add_filter(name='strong', method=strong)
+        self.plugin.add_filter(name='address', method=address)
+
+        self.assertEquals(self.plugin.apply_filter('strong', 'strong method'), strong('strong method'))
+        self.assertEquals(self.plugin.apply_filter('strong', 'strong method'), '<strong>strong method</strong>')
+
+        mixed_result = self.plugin.apply_filter('address', self.plugin.apply_filter('strong', 'strong + address method'))
+
+        self.assertEquals(mixed_result, address(strong('strong + address method')))
+        self.assertEquals(mixed_result, '<address><strong>strong + address method</strong></address>')
+
+    def test_add_action(self):
+        def puts(value):
+            print(value)
+
+        self.plugin.add_action(name='puts', method=puts)
+
+        self.assertEquals(self.plugin.actions['puts'], puts)
+
+    def test_do_action(self):
+        def puts(value):
+            print(value)
+
+        self.plugin.add_action(name='puts', method=puts)
+
+        with captured_output() as (out, err):
+            self.plugin.do_action('puts', 'put method')
+            self.assertEquals(out.getvalue().strip(), 'put method')
