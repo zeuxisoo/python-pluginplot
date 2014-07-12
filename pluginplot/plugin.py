0cd1311d3971bb4813bbf5986b7988ae43f6da59
Zeuxis Lo <seekstudio@gmail.com>
2014-07-13 02:16:56 +0800

    support register all plots when pass folder arguments

--- pluginplot/plugin.py
+++ pluginplot/plugin.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+import imp
+
 __all__ = ['Plugin']
 
 class PluginException(Exception): pass
@@ -9,9 +13,10 @@ class ActionError(PluginException): pass
 
 class Plugin(object):
 
-    def __init__(self):
+    def __init__(self, folder = None):
         self.actions = {}
         self.filters = {}
+        self.folder  = folder
 
     def add_filter(self, name, method):
         self.filters[name] = method
@@ -37,3 +42,37 @@ class Plugin(object):
 
     def register_plot(self, plot):
         plot.register(self)
+
+    def register_plots(self, module_name="pluginplot.plugins"):
+        if not self.folder:
+            raise PluginException("Plugin.folder can not be None")
+        else:
+            # Register base module
+            self.register_module(module_name)
+
+            for folder_path, folder_names, filenames in os.walk(self.folder):
+                # Register plot module under base module
+                # - when folder set to path/to/plots
+                # - like path/to/plots/bar > module_name.bar
+                for folder_name in folder_names:
+                    self.register_module("{0}.{1}".format(module_name, folder_name))
+
+                # Load each plot file
+                # - like path/to/plots/bar/foo.py > module_name.bar.foo
+                for filename in filenames:
+                    if filename.endswith('.py'):
+                        # path/to/plots/bar/foo.py > bar/foo.py > bar.foo > module_name.bar.foo
+                        full_plot_path   = os.path.join(folder_path, filename)
+                        base_plot_path   = os.path.relpath(full_plot_path, self.folder)
+                        base_plot_name   = os.path.splitext(base_plot_path)[0].replace(os.path.sep, '.')
+                        module_source_name = "{0}.{1}".format(module_name, base_plot_name)
+
+                        loaded_module = imp.load_module(
+                            module_source_name,
+                            *imp.find_module(os.path.splitext(filename)[0], [folder_path])
+                        )
+
+                        self.register_plot(loaded_module.plot)
+
+    def register_module(self, name):
+        sys.modules[name] = imp.new_module(name)
